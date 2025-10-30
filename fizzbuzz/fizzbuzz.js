7f73e8a6c88f1b9d4544cc86ba6a5eeb3ef16d6a
mayaxya <maya.mariottini@gmail.com>
2025-10-30 21:04:06 +0100

    fizzbuzz function and test ok for 1 2 3 5 15

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,5 +1,17 @@
 function fizzbuzz (x) {
-	return (x)
+	let multip15 =x/15
+	let multip5 = x/5
+	let multip3 = x/3
+	if (Number.isInteger(multip15) === true) {
+		return("fizzbuzz")
+	}
+	else if (Number.isInteger(multip5) === true) {
+		return("buzz")
+	}
+	else if (Number.isInteger(multip3) === true) {
+		return("fizz")
+	}
+	else return (x)
 }
 
 module.exports = fizzbuzz;
--- fizzbuzz/fizzbuzz.test.js
+++ fizzbuzz/fizzbuzz.test.js
@@ -1,6 +1,22 @@
 
 const fizzbuzz = require('./fizzbuzz');
+
 test("donne suite nombre jusqu'a 1", ()=>{
 	expect(fizzbuzz(1)).toBe(1)
 })
 
+test("pour 2",()=>{
+	expect(fizzbuzz(2)).toBe(2)
+})
+
+test("pour 3",()=>{
+	expect(fizzbuzz(3)).toBe("fizz")
+})
+
+test("pour 5",()=>{
+	expect(fizzbuzz(5)).toBe("buzz")
+})
+
+test("pour 15",()=>{
+	expect(fizzbuzz(15)).toBe("fizzbuzz")
+})
