eca6f31f6c7f6310ab276f76a5392386bed60644
mayaxya <maya.mariottini@gmail.com>
2025-11-05 12:31:09 +0100

    call fizzbuzz using array ok

--- fizzbuzz/callfizzbuzz.js
+++ fizzbuzz/callfizzbuzz.js
@@ -1,8 +1,12 @@
 const fizzbuzz = require('./fizzbuzz');
 
 function main (x) {
-	while (i<x){
-		return (fizzbuzz(i))
+	const arr = []
+	let i = 1
+	while (i<=x){
+		arr.push(fizzbuzz(i))
 		i+=1
 	}
+	return (arr)
 }
+console.log(main(15))
